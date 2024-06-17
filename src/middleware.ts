dbc8ee4fc6baff9933c789981a77bc1e43ae5abf
Ritik Tiwari <dev.ritiktiwari@gmail.com>
2024-06-17 19:10:53 +0530

    FIX: update quotes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
     const token = await getToken({ req });
     const isAuthenticated = !!token;
     const url = req.nextUrl.clone();
 
-    const protectedRoutes = ['/account', '/admin'];
-    const authRestrictedRoutes = ['/auth'];
+    const protectedRoutes = ["/account", "/admin"];
+    const authRestrictedRoutes = ["/auth"];
 
     if (isAuthenticated && authRestrictedRoutes.some(route => url.pathname.startsWith(route))) {
-        url.pathname = '/';
+        url.pathname = "/";
         return NextResponse.redirect(url);
     }
 
     if (!isAuthenticated && protectedRoutes.some(route => url.pathname.startsWith(route))) {
-        url.pathname = '/auth';
+        url.pathname = "/auth";
         return NextResponse.redirect(url);
     }
 
@@ -23,5 +23,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
