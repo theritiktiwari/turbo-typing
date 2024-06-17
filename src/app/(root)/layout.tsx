d63460b3ee4bc3471a50c68b18e9d5d2ff658f0b
Ritik Tiwari <dev.ritiktiwari@gmail.com>
2024-06-18 00:13:22 +0530

    FEAT: add footer

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Navbar />
             {children}
+            <Footer />
         </>
     );
 }
---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Link from "next/link";
+import { CircleDollarSign, CodeXml, GlobeLock, Mail, NotepadText, ShieldCheck } from "lucide-react";
+import { ThemeToggle } from "@/components/shared/ThemeToggle";
+import { Separator } from "@/components/ui/separator";
+
+export default function Footer() {
+    const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? "Turbo Typing";
+
+    const copyright = (year: number) => {
+        const currentYear = new Date().getFullYear();
+        if (year > currentYear) return currentYear;
+        return year === currentYear ? year : `${year}-${currentYear % 100}`;
+    }
+
+    return (
+        <footer className="footer">
+            <div className="flex justify-between items-start md:items-center">
+                <div className="flex-center gap-5 md:gap-7">
+                    <ul className={"text-xs flex-items-start flex-col md:flex-row gap-5 md:gap-7"}>
+                        {/* TODO: Add modal to contact info */}
+                        <Link href={"/"}>
+                            <li className="flex-center gap-1">
+                                <Mail size={"1rem"} /> Contact
+                            </li>
+                        </Link>
+
+                        {/* TODO: Add modal to support links */}
+                        <Link href={"/"}>
+                            <li className="flex-center gap-1">
+                                <CircleDollarSign size={"1rem"} /> Support
+                            </li>
+                        </Link>
+
+                        <Link href={"https://github.com/theritiktiwari/turbo-typing"} target="_blank">
+                            <li className="flex-center gap-1">
+                                <CodeXml size={"1rem"} /> GitHub
+                            </li>
+                        </Link>
+                    </ul>
+
+                    <ul className={"text-xs flex-items-start flex-col md:flex-row gap-5 md:gap-7"}>
+                        <Link href={"/pages/terms-of-service"}>
+                            <li className="flex-center gap-1">
+                                <NotepadText size={"1rem"} /> Terms
+                            </li>
+                        </Link>
+                        <Link href={"/pages/security-policy"}>
+                            <li className="flex-center gap-1">
+                                <ShieldCheck size={"1rem"} /> Securtity
+                            </li>
+                        </Link>
+                        <Link href={"/pages/privacy-policy"}>
+                            <li className="flex-center gap-1">
+                                <GlobeLock size={"1rem"} /> Privacy
+                            </li>
+                        </Link>
+                    </ul>
+                </div>
+
+                {/* toggle themes */}
+                <ThemeToggle />
+            </div>
+            <Separator className="my-4 bg-muted-foreground" />
+            <div className="text-xs text-center">
+                <p>&copy; {copyright(2024)} {APP_NAME}. All rights reserved.</p>
+                <p>Made with ❤️ by <a href="https://theritiktiwari.vercel.app" target="_blank" rel="noopener noreferrer" className="link">Ritik Tiwari</a>.</p>
+            </div>
+        </footer>
+    );
+}
