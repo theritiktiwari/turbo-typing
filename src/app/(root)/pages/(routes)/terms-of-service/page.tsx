e4478a4ce971692f14e8879a7695a09190c8f1c9
Ritik Tiwari <dev.ritiktiwari@gmail.com>
2024-06-18 16:00:52 +0530

    FEAT: add terms of service page

---
+++ src/app/(root)/pages/(routes)/terms-of-service/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { termsOfService } from "@/constants/terms-of-service";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Terms of Service",
+};
+
+const Page = () => {
+    const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? "Turbo Typing";
+
+    return (
+        <>
+            <div className="space-y-3">
+                <h1 className='page-heading text-left'>Terms of Service - {APP_NAME} </h1>
+            </div>
+
+            <div className="space-y-3">
+                <h1 className='page-heading'>Introduction</h1>
+                <p className='page-description'>These Terms of Service govern your use of our website and services. By accessing or using our services, you agree to be bound by these terms.</p>
+            </div>
+
+            <div className="space-y-3">
+                {termsOfService?.map((term, index) => (
+                    <div key={index} className="space-y-3">
+                        <h2 className='page-subheading capitalize'>{term.heading}</h2>
+                        {term.content.map((content, index) => (
+                            Array.isArray(content) ? <ul className='space-y-2'>
+                                {content.map((subContent, contentIndex) => (
+                                    <li className="page-list-item" key={contentIndex}>{subContent}</li>
+                                ))}
+                            </ul> : <p key={index}>{content}</p>
+                        ))}
+                    </div>
+                ))}
+            </div>
+
+            <div className="space-y-3">
+                <p className='page-description'>These terms of service were last updated on June 18, 2024.</p>
+            </div>
+        </>
+    )
+}
+
+export default Page
---
+++ src/constants/terms-of-service.ts
@@ -0,0 +1,88 @@
+const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? "Turbo Typing";
+const SUPPORT_EMAIL = process.env.NEXT_PUBLIC_SUPPORT_EMAIL ?? "<SUPPORT_EMAIL>";
+
+export const termsOfService = [
+    {
+        heading: "Section 1: Agreement",
+        content: [
+            `This website is operated by [${APP_NAME}]. Throughout the site, the terms "we", "us" and "our" refer to [${APP_NAME}]. [${APP_NAME}] offers this website, including all information, tools and services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.`,
+            `By visiting our site, you engage in our "Service" and agree to be bound by the following terms and conditions ("Terms of Service"), including those additional terms and conditions and policies referenced herein or available by hyperlink. These Terms of Service apply to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, or contributors of content.`,
+            `Please read these Terms of Service carefully before accessing or using our website. By accessing or using any part of the site, you agree to be bound by these Terms of Service. If you do not agree to all the terms and conditions of this agreement, then you may not access the website or use any services. If these Terms of Service are considered an offer, acceptance is expressly limited to these Terms of Service.`,
+            `Any new features or tools which are added to the current store shall also be subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes.`
+        ]
+    },
+    {
+        heading: "Section 2: Limitations",
+        content: [
+            `You are responsible for your account's security and all activities on your account. You must not, in the use of this site, violate any applicable laws, including, without limitation, copyright laws, or any other laws regarding the security of your personal data, or otherwise misuse this site.`,
+            `While using our services, you agree that you will not:`,
+            [
+                `access or attempt to access any service by any means other than as permitted in the agreement, or operating the services on any computers or accounts which you do not have permission to operate;`,
+                `violate the privacy of any third party, such as by posting another person's personal information without their consent;`,
+                `falsely impersonate any person or entity, including any of our contributors, misrepresent your identity or the site's purpose, or falsely associate yourself with us;`,
+                `access the services in ways that exceed your authorization;`,
+                `harass, abuse, threaten, or incite violence towards any individual or group, including other users and our contributors;`,
+                `use our servers for any form of excessive automated bulk activity (e.g., spamming), or rely on any other form of unsolicited advertising or solicitation through our servers or services;`,
+                `attempt to disrupt or tamper with our servers in ways that could a) harm our website or services or b) place undue burden on our servers;`,
+                `facilitate or encourage any violations of the agreement or interfere with the operation, appearance, security, or functionality of the services; or`,
+                `use the services in any manner that is harmful to minors.`
+            ],
+            `You agree that you will not upload, post, host, or transmit any content that:`,
+            [
+                `is degrading to others on the basis of gender, race, class, ethnicity, national origin, religion, sexual preference, orientation, or identity, disability, or other classification, or otherwise represents or condones content that: is hate speech, discriminating, threatening, or pornographic; incites violence; or contains nudity or graphic or gratuitous violence;`,
+                `is libelous, defamatory, or fraudulent;`,
+                `is or contains sexually obscene content;`,
+                `is discriminatory or abusive toward any individual or group;`,
+                `is unlawful or promotes unlawful activities;`,
+                `violates any person's right to privacy or publicity, or otherwise solicits, collects, or publishes data, including personal information and login information, about other Users without consent or for unlawful purposes in violation of any applicable international, federal, state, or local law, statute, ordinance, or regulation; or`,
+                `contains or installs any active malware or exploits/uses our platform for exploit delivery (such as part of a command or control system); or`,
+                `infringes on any proprietary right of any party, including patent, trademark, trade secret, copyright, right of publicity, or other rights.`,
+            ],
+            `Without limiting the foregoing, you will not transmit or post any content anywhere on the services that violates any laws. We absolutely do not tolerate engaging in activity that significantly harms our users. We will resolve disputes in favor of protecting our users as a whole.`,
+
+            `You shouldn't use bots or access our services in malicious or prohibited ways. While accessing or using the services, you may not:`,
+            [
+                `tamper with or use non-public areas of the services, or the computer or delivery systems of ${APP_NAME} and/or its service providers;`,
+                `employ misleading email or IP addresses or forged headers or otherwise manipulated identifiers in order to disguise the origin of any content transmitted to or through the services;`,
+                `create manual requests to ${APP_NAME} servers;`,
+                `use bots, hacks, or cheats while using our site;`,
+                `scrape the services, scrape Content from the services, or use automated means, including spiders, robots, crawlers, data mining tools, or the like to download data from the services or otherwise access the services;`,
+                `probe, scan, or test any system or network (particularly for vulnerabilities), or otherwise attempt to breach or circumvent any security or authentication measures, or search or attempt to access or search the services by any means (automated or otherwise) other than through our currently available, published interfaces that are provided by us (and only pursuant to those terms and conditions);`,
+                `use the services to send altered, deceptive, or false source-identifying information, including, without limitation, by forging TCP-IP packet headers or e-mail headers; or`,
+                `interfere with, or disrupt or attempt to interfere with or disrupt, the access of any User, host, or network, including, without limitation, by sending a virus to, spamming, or overloading the services, or by scripted use of the services in such a manner as to interfere with or create an undue burden on the services.`
+            ]
+        ]
+    },
+    {
+        heading: "Section 3: Disclaimer",
+        content: [
+            `Excluding the explicitly stated warranties within these terms, we only offer our services on an 'as-is' basis. Your access to and use of the services or any content is at your own risk. You understand and agree that the services and content are provided to you on an 'as is,' 'with all faults,' and 'as available' basis. Without limiting the foregoing, to the full extent permitted by law, we disclaim all warranties, express or implied, including without limitation warranties of merchantability, fitness for a particular purpose, or non-infringement. To the extent such disclaimer conflicts with applicable law, the scope and duration of any applicable warranty will be the minimum permitted under such law. We make no representations, warranties, or guarantees as to the reliability, timeliness, quality, suitability, availability, accuracy, or completeness of any kind with respect to the services, including any representation or warranty that the use of the services will`,
+            [
+                `be timely, uninterrupted, or error-free, or operate in combination with any other hardware, software, system, or data,`,
+                `meet your requirements or expectations,`,
+                `be free from errors or that defects will be corrected, or`,
+                `be free of viruses or other harmful components.`,
+            ],
+            `We also make no representations or warranties of any kind with respect to content; user content is provided by and is solely the responsibility of the respective user providing that content. No advice or information, whether oral or written, obtained from us or through the services, will create any warranty not expressly made herein. We do not warrant, endorse, guarantee, or assume responsibility for any user content on the services or any hyperlinked website or third- party service, and we will not be a party to or in any way be responsible for transactions between you and third parties. If applicable law does not allow the exclusion of some or all of the above implied or statutory warranties to apply to you, the above exclusions will apply to you to the fullest extent permitted by applicable law.`
+        ]
+    },
+    {
+        heading: "Section 4: Links",
+        content: [
+            `${APP_NAME} is not responsible for the contents of any linked sites. The use of any linked website is at the user's own risk.`
+        ]
+    },
+    {
+        heading: "Section 5: Changes",
+        content: [
+            `${APP_NAME} may revise these Terms of Service for its website at any time without prior notice. By using this website, you are agreeing to be bound by the current version of these Terms of Service.`
+        ]
+    },
+    {
+        heading: "Section 6: Contact Information",
+        content: [
+            `If you have any questions about our privacy policy, the data we hold on you, or you would like to exercise one of your data protection rights, please do not hesitate to contact us.`,
+            `Questions about the Terms of Service should be sent to us at ${SUPPORT_EMAIL} `
+        ]
+    }
+];
