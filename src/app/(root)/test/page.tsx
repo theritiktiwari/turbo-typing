a825a2b19db9f4e8f3f53246c563fff3fd0ee61a
Ritik Tiwari <dev.ritiktiwari@gmail.com>
2024-06-22 11:31:19 +0530

    FEAT: add results

--- src/app/(root)/test/page.tsx
+++ src/app/(root)/test/page.tsx
@@ -4,9 +4,15 @@ import { useSetting } from "@/hooks/use-setting";
 import { TypingTest } from "@/components/shared/TypingTest";
 import { useEffect, useState, useRef } from "react";
 import { Button } from "@/components/ui/button";
+import languageList from "@/constants/languages/_list.json";
+
+interface JSONList {
+    [key: string]: string;
+}
 
 export default function Page() {
     const setting = useSetting();
+    const languages: JSONList = languageList;
     const timerRef = useRef<NodeJS.Timeout | null>(null);
 
     const [isMounted, setIsMounted] = useState(false);
@@ -16,13 +22,21 @@ export default function Page() {
     const [words, setWords] = useState<string[]>([]);
     const [correctWords, setCorrectWords] = useState<string[]>([]);
     const [mistakes, setMistakes] = useState(0);
+    const [result, setResult] = useState({
+        wordTyped: 0,
+        correctCharacters: 0,
+        accuracy: 0,
+        language: "",
+        time: "",
+        speed: ""
+    });
 
     const loadWords = async () => {
         try {
             const { language } = setting;
             const module = await import(`@/constants/languages/${language}.json`);
             const wordsArray = module.default[setting.difficulty];
-
+            wordsArray.push(...wordsArray);
             wordsArray.sort(() => Math.random() - 0.5);
 
             const wordsToParagraph = wordsArray.join(" ");
@@ -44,6 +58,10 @@ export default function Page() {
         if (time === 0 && timerRef.current) {
             clearInterval(timerRef.current as NodeJS.Timeout);
             timerRef.current = null;
+            saveResult();
+        }
+        if (time === 0) {
+            saveResult();
         }
     }, [time]);
 
@@ -76,6 +94,43 @@ export default function Page() {
         }
     };
 
+    const saveResult = () => {
+        // show the result to the user 
+        const wordTyped = correctWords.length;
+        const charactersTyped = characters.filter((char) => char !== " ").length;
+        const correctCharacters = correctWords.reduce((acc, word) => acc + word.length, 0);
+        const accuracy = Math.floor(100 - ((mistakes / charactersTyped) * 100));
+        const language = languages[setting.language];
+        const time = setting.time;
+        const unit = setting.unit;
+        const wpm = Math.round(((charactersTyped - mistakes) / 5) / (setting.time / 60));
+        const cpm = Math.round((charactersTyped - mistakes) / (setting.time / 60));
+        const wps = Math.round((charactersTyped - mistakes) / setting.time);
+        const cps = Math.round((charactersTyped - mistakes) / setting.time);
+
+        let speed = "";
+        if (unit === "WPM") {
+            speed = `${wpm} ${unit}`;
+        } else if (unit === "CPM") {
+            speed = `${cpm} ${unit}`;
+        } else if (unit === "WPS") {
+            speed = `${wps} ${unit}`;
+        } else if (unit === "CPS") {
+            speed = `${cps} ${unit}`;
+        }
+
+        setResult({
+            wordTyped,
+            correctCharacters,
+            accuracy,
+            language,
+            time: `${time} Sec`,
+            speed
+        });
+
+        //TODO: Save the result to the database if loggedin 
+    }
+
     return (
         <div className="typing-test__box">
             <div className="typing-box__top">
@@ -105,6 +160,50 @@ export default function Page() {
                     }}
                 />
             )}
+
+            {time === 0 && (
+                <div className="typing-test__results">
+                    <div className="results__box">
+                        <span>Language</span>
+                        <span className="md:hidden">:</span>
+                        <span className="font-medium md:text-2xl">{result.language}</span>
+                    </div>
+
+                    <div className="results__box">
+                        <span>Speed</span>
+                        <span className="md:hidden">:</span>
+                        <span className="font-medium md:text-2xl">{result.speed}</span>
+                    </div>
+
+                    <div className="results__box">
+                        <span>Accuracy</span>
+                        <span className="md:hidden">:</span>
+                        <span className="font-medium md:text-2xl">{`${result.accuracy} %`}</span>
+                    </div>
+
+                    <div className="results__box">
+                        <span>Test Time</span>
+                        <span className="md:hidden">:</span>
+                        <span className="font-medium md:text-2xl">{result.time}</span>
+                    </div>
+
+                    <div className="results__box">
+                        <span>Characters</span>
+                        <span className="md:hidden">:</span>
+                        <span className="font-medium md:text-2xl">
+                            <span className="text-success">{result.correctCharacters}</span>
+                            <span> / </span>
+                            <span className="text-destructive">{mistakes}</span>
+                        </span>
+                    </div>
+                    <div className="results__box">
+                        <span>Total Words</span>
+                        <span className="md:hidden">:</span>
+                        <span className="font-medium md:text-2xl">{result.wordTyped}</span>
+                    </div>
+                </div>
+            )}
+
         </div>
     );
 }
